6dce8df52e5ca05a79aeac9501afc63d1b20b03d
Komamitsu <komamitsu@gmail.com>
2009-09-28 23:16:55 +0900

    use process.

--- rclass.rb
+++ rclass.rb
@@ -1,5 +1,5 @@
-require 'thread'
 require 'drb/drb'
+require 'socket'
 
 def rclass(klass, &blk)
   cls = nil
@@ -14,20 +14,27 @@ def rclass(klass, &blk)
   cls.module_eval(&blk)
   cls.module_eval do
     def self.new(*args)
-      q = Queue.new
+      remote_uri = nil
 
-      Thread.new do
-        obj = self.allocate
-        obj.__send__(:initialize, *args)
-        DRb.start_service('druby://localhost:0', obj)
-        q << DRb.uri
-        sleep
+      TCPServer.open(0) do |server|
+        thread_for_remote_uri = Thread.new do
+          remote_uri = server.accept.read
+        end
+
+        fork do
+          obj = self.allocate
+          obj.__send__(:initialize, *args)
+          DRb.start_service('druby://:0', obj)
+          TCPSocket.open('127.0.0.1', server.addr[1]) do |sock|
+            sock.write(DRb.uri)
+          end
+          sleep
+        end
+
+        thread_for_remote_uri.join
       end
 
-      uri = q.shift
-      DRb.primary_server = nil
-      robj = DRbObject.new_with_uri(uri)
-      robj
+      DRbObject.new_with_uri(remote_uri)
     end
   end
 end
